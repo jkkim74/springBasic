0a89f75d8955e66c8577e829f1cd4fdb5c67f731
jack <jack68@naver.com>
2021-08-01 16:04:19 +0900

    test22

--- src/main/java/hello/spring/repository/MemberRepository.java
+++ src/main/java/hello/spring/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package hello.spring.repository;
 
 import hello.spring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository {
 
     Member save(Member member);
